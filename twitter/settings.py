ea3476c3e4f44178c77aef31e7314679d314d151
dev <mannycandidier@gmail.com>
2023-10-20 20:29:42 +0800

    django environ

--- twitter/settings.py
+++ twitter/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
 import environ
+import os
 from pathlib import Path
 
 env = environ.Env(
